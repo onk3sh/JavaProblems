685b93cc008f9de144d99851d9ed35eb29981154
Onkesh Bansal <onk3sh@users.noreply.github.com>
2018-11-27 16:33:04 +0530

    Init Commit

--- src/arrays/TestClass2.java
+++ src/arrays/TestClass2.java
@@ -1,5 +1,8 @@
 package arrays;
 
+import java.util.*;
+import java.util.Map.Entry;
+
 public class TestClass2 {
 	
 //	Sort an array based on sorting order defined by another array:
@@ -11,6 +14,17 @@ public class TestClass2 {
 //		  
 //		  condition = in place***
 
+	public static int getFirstPositionInArray(int[] arr, int start, int input) {
+		int pos = -1;
+		for(int i = start; i<arr.length;i++) {
+			if(arr[i] == input) {
+				pos = i;
+				break;
+			}
+		}
+		return pos;
+	}
+	
 	public static void swap(int[] arr, int i, int j) {
 		int temp = arr[i];
 		arr[i] = arr[j];
@@ -18,19 +32,23 @@ public class TestClass2 {
 	}
 	
 	public static void main(String[] args) {
-		int[] arr1={5,4,2,3,1};
-		int[] referenceArray={2,4,5,3,1};
+		int[] arr={5,4,2,4,2,3,2,3,5,1,2,3,4,5};
+		int[] refArr={2,4,5,3,1};
+		int i = 0, temp = 0, j = 0;
 		
-		for(int i = 0; i<referenceArray.length; i++) {
-			for(int j = 0; j<arr1.length; j++) {
-				if(arr1[j] == referenceArray[i]) {
-					swap(arr1,i,j);
-				}
+		while(i<arr.length) {
+			while(temp != -1) {
+				temp = getFirstPositionInArray(arr, i, refArr[j]);
+				if(temp == -1) break;
+				swap(arr,i,temp);
+				i++;
 			}
+			j++;
+			temp = 0;
 		}
 		
-		for(int i = 0; i<arr1.length;i++) {
-			System.out.print(arr1[i]);
+		for(i = 0; i<arr.length;i++) {
+			System.out.print(arr[i]);
 		}
 		
 	}
---
+++ src/stacks/ImplementStackUsingArray.java
@@ -0,0 +1,56 @@
+package stacks;
+
+public class ImplementStackUsingArray {
+
+	private static int MAX = 10;
+	private int[] arr;
+	private int currentPos;
+	
+	public ImplementStackUsingArray() {
+		arr	= new int[MAX];
+		arr[0] = -1;
+		currentPos = 0;
+	}
+	
+	public void push(int value) {
+		currentPos++;
+		if(currentPos > MAX) {
+			System.out.println("Stack Overflow!");
+			currentPos = MAX;
+		}
+		arr[currentPos] = value;
+	}
+	
+	public void pop() {
+		arr[currentPos] = 0;
+		currentPos--;
+		if(currentPos < 0 || arr[currentPos] == -1) {
+			System.out.println("Stack is empty. Cannot pop further");
+			currentPos = 1;
+		}
+	}
+	
+	public Boolean isEmpty() {
+		if(currentPos < 0 || (currentPos == 0  && arr[currentPos] == -1) )
+			return true;
+		else
+			return false;
+	}
+	
+	public void peek() {
+		System.out.println(arr[currentPos]);
+	}
+	
+	public static void main(String[] args) {
+		ImplementStackUsingArray s = new ImplementStackUsingArray();
+		s.push(1);
+		s.push(2);
+//		s.push(3);
+		s.pop();
+		s.pop();
+//		s.pop();
+		s.peek();
+	}
+	
+}
+
